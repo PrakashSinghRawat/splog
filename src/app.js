d49cd2512fc1e05913be200ded45aeb2cdedd473
Prakash Singh <prakashsinghrawat05@gmail.com>
2021-01-08 23:24:47 +0530

    updated with port

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const app = express();
 const bodyParser = require('body-parser')
 var routes = require('./config/routes');
 // var hostName="splog.sdssoftltd.co.uk"
-process.env.PORT = 8080;
+let PORT = process.env.PORT||8080;
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json()) 
@@ -12,6 +12,6 @@ app.set('view engine', 'ejs')
 
 app.use('/', routes)
 
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`Example app listening at https://zoom-api-server.herokuapp.com:${process.env.PORT}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening at https://zoom-api-server.herokuapp.com:${PORT}`)
 })
